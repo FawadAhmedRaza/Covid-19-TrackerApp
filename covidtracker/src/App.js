a911093a500e5262aecf33627edb79be7b7342fd
FawadAhmedRaza <68689723+FawadAhmedRaza@users.noreply.github.com>
2021-02-20 13:38:00 +0500

    .

---
+++ covidtracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Ckeck from './Components/header'
+import Check from './Components/header';
+function App() {
+  return (
+    <div className="App">
+     <Check></Check>
+    </div>
+  );
+}
+
+export default App;
